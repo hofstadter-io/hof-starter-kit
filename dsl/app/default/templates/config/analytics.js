f4766123f32b2eb6d37b09507eba5e9f0d522841
Tony Worm <verdverm@gmail.com>
2018-09-30 11:08:40 -0600

    breakup geb-gen files; move analytics to templates

--- dsl/app/default/templates/config/analytics.js
+++ dsl/app/default/templates/config/analytics.js
@@ -1,6 +1,6 @@
 export default {
   ga: {
-    trackingId: 'UA-000000-01'
+    trackingId: '{{DslContext.config.gaTracking}}'
   },
   apolloEngine: {
     key: 'service:sysgears-Apollo-Universal-Starter-Kit:mZvnMtAgDE93vAtk8Cj-1Q' // Set your Apollo Engine key here
