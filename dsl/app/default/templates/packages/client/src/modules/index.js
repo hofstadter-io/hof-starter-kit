a749043b5142880f0c8e339ca81d484880c12bf5
Tony Worm <verdverm@gmail.com>
2018-10-09 17:40:58 -0600

    BOOOOM, nuking things

--- dsl/app/default/templates/packages/client/src/modules/index.js
+++ dsl/app/default/templates/packages/client/src/modules/index.js
@@ -1,13 +1,7 @@
 import defaultRouter from './defaultRouter';
 import i18n from './i18n';
 
-import counter from './counter';
-import post from './post';
-import upload from './upload';
-import pagination from './pagination';
-
 import user from './user';
-import subscription from './subscription';
 
 import pages from './pages';
 import contact from './contact';
@@ -18,13 +12,10 @@ import Feature from './connector';
 
 export default new Feature(
   defaultRouter,
-  counter,
-  post,
-  upload,
+
   user,
-  subscription,
+
   contact,
-  pagination,
   pages,
   pageNotFound,
   i18n
--- dsl/app/default/templates/packages/server/src/modules/index.ts
+++ dsl/app/default/templates/packages/server/src/modules/index.ts
@@ -1,10 +1,8 @@
 import cookies from './cookies';
 import i18n from './i18n';
-import counter from './counter';
-import post from './post';
-import upload from './upload';
+
 import user from './user';
-import subscription from './subscription';
+
 import contact from './contact';
 import mailer from './mailer';
 import graphqlTypes from './graphqlTypes';
@@ -12,4 +10,13 @@ import './debug';
 
 import Feature from './connector';
 
-export default new Feature(cookies, i18n, counter, post, upload, user, subscription, contact, mailer, graphqlTypes);
+export default new Feature(
+  cookies,
+  i18n,
+
+  user,
+
+  contact,
+  mailer,
+  graphqlTypes
+);
