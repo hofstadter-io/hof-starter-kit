f294941f4a5fd4b6ed4b394ac06cab550edea43f
Tony Worm <verdverm@gmail.com>
2018-12-22 19:51:00 -0700

    add loop over app migrations in the type migration templates

---
+++ dsl/type/default/partials/db/mig-down.js
@@ -0,0 +1,1 @@
+// mig DOWN - {{MIG_ID}}
--- dsl/type/default/templates/db/migrations.js
+++ dsl/type/default/templates/db/migrations.js
@@ -1,11 +1,33 @@
 {{#with DslContext as |TYPE|}}
+{{#getdsl "app" true}}{{#with . as |APP|}}
+// {{APP.name}} {{APP.versionMig}}
 exports.up = function(migId, knex, Promise) {
   var migs = [];
   var mig = null;
 
-  {{> db/table-create-type-table.js}}
+  switch (migID) {
 
-  {{> db/table-create-rels-table.js}}
+    {{#each (intloop APP.versionMig) as |MIG_ID|}}
+    case {{MIG_ID}}:
+
+      {{> db/mig-up.js}}
+
+      break;
+
+    {{/each}}
+
+    case "next":
+
+      {{> db/next-up.js}}
+
+      break
+  }
+
+  /*
+  if (migId === "next") {
+
+  }
+  */
 
   return Promise.all(migs);
 };
@@ -13,12 +35,27 @@ exports.up = function(migId, knex, Promise) {
 exports.down = function(migId, knex, Promise) {
   var migs = [];
 
-  {{> db/table-drop-type-table.js}}
+  switch (migID) {
 
-  {{> db/table-drop-rels-table.js}}
+    {{#each (intloop APP.versionMig) as |MIG_ID|}}
+    case {{MIG_ID}}:
+
+      {{> db/mig-down.js}}
+
+      break;
+
+    {{/each}}
+
+    case "next":
+
+      {{> db/next-down.js}}
+
+      break
+  }
+
 
   return Promise.all(migs);
 };
 
+{{/with}}{{/getdsl}}
 {{/with}}
-
