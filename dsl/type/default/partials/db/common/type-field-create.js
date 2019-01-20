0c1eae86a34fdb69793bc4dd6e23afb6a05a6cab
Tony Worm <verdverm@gmail.com>
2019-01-20 01:21:36 -0700

    field (un-)create/delete seemingly finished

--- dsl/type/default/partials/db/common/type-field-create.js
+++ dsl/type/default/partials/db/common/type-field-create.js
@@ -1,36 +1,4 @@
 // db/common/type-field-create.js
 {{#with MIG.value as |FIELD|}}
-{{#if (eq FIELD.type "string")}}
-table.string('{{snake FIELD.name}}'{{#if FIELD.length}}, {{FIELD.length}}{{/if}});
-{{else if (eq FIELD.type "text")}}
-table.text('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "json")}}
-table.json('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "jsonb")}}
-table.jsonb('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "boolean")}}
-table.boolean('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "integer")}}
-table.integer('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "decimal")}}
-table.decimal('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "date")}}
-table.date('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "time")}}
-table.time('{{snake FIELD.name}}')
-{{else if (eq FIELD.type "datetime")}}
-table.dateTime('{{snake FIELD.name}}')
-{{/if}}
-{{#if FIELD.default}}
-  .defaultsTo({{FIELD.default}})
-{{/if}}
-{{#if FIELD.unique}}
-  .unique()
-{{/if}}
-{{#if FIELD.nullable}}
-  .nullable()
-{{/if}}
-{{#if FIELD.notNullable}}
-  .notNullable()
-{{/if}}
+{{> db/common/field-defines.js}}
 {{/with}}
