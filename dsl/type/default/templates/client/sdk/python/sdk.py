4a11a7ee70f17494eda2eb66292dec1f9636dbff
Philip Dexter <philip.dexter@gmail.com>
2019-02-06 15:13:01 +0200

    Update python sdk with simple graphql parse and post logic

--- dsl/type/default/templates/client/sdk/python/sdk.py
+++ dsl/type/default/templates/client/sdk/python/sdk.py
@@ -3,27 +3,34 @@ hof-starter-kit Python SDK
 """
 
 import json
+import os
 import requests
 
-def get_jwt_token(username, password):
-  response = requests.post(
-      'http://192.168.1.103:8081/graphql',
-      headers={
-        'content-type': 'application/json',
-      },
-      data='[{"operationName":"login","variables":{"input":{"usernameOrEmail":"' + username + '","password":"' + password + '"}},"query":"mutation login($input: LoginUserInput!) {  login(input: $input) {    tokens {      accessToken      refreshToken      __typename    }    user {      ...UserInfo      __typename    }    errors {      field      message      __typename    }    __typename  }}fragment UserInfo on User {  id  username  role  isActive  email  profile {    ...UserProfileInfo    __typename  }  auth {    apikey {      apikey      __typename    }    certificate {      serial      __typename    }    facebook {      fbId      displayName      __typename    }    google {      googleId      displayName      __typename    }    github {      ghId      displayName      __typename    }    linkedin {      lnId      displayName      __typename    }    __typename  }  __typename}fragment UserProfileInfo on UserProfile {  firstName  middleName  lastName  title  suffix  fullName  __typename}"}]'
-      )
-  data = json.loads(response.text)
-  access_token = data[0]['data']['login']['tokens']['accessToken']
-  return access_token
+API_KEY = "changeme"
 
-def get_users(jwt):
+user_query_file = "/home/philip/go/src/github.com/hofstadter-io/studios-app/output/packages/client/src/modules/user/graphql/UsersQuery.graphql"
+
+def read_query_file(query_file):
+  lines = None
+  with open(query_file, 'r') as file:
+    lines = file.readlines()
+  for line in lines:
+    if line.startswith('#import'):
+      to_import = line.split()[1]
+      lines = read_query_file(os.path.dirname(query_file) + '/' + to_import.strip('"')) + lines
+
+  return lines
+
+def run_graphql(query_file):
+  lines = ''.join(read_query_file(query_file))
+
   response = requests.post(
       'http://192.168.1.103:8081/graphql',
       headers={
         'content-type': 'application/json',
-        'authorization': f'Bearer {jwt}',
+        'authorization': f'Bearer {API_KEY}',
       },
-      data='[{"operationName":"users","variables":{"orderBy":{"column":"","order":""},"filter":{"searchText":"","role":"","isActive":true}},"query":"query users($orderBy: OrderByUserInput, $filter: FilterUserInput) {  users(orderBy: $orderBy, filter: $filter) {    ...UserInfo    __typename  }}fragment UserInfo on User {  id  username  role  isActive  email  profile {    ...UserProfileInfo    __typename  }  auth {    apikey {      apikey      __typename    }    certificate {      serial      __typename    }    facebook {      fbId      displayName      __typename    }    google {      googleId      displayName      __typename    }    github {      ghId      displayName      __typename    }    linkedin {      lnId      displayName      __typename    }    __typename  }  __typename}fragment UserProfileInfo on UserProfile {  firstName  middleName  lastName  title  suffix  fullName  __typename}"}]'
+      json={'query': lines}
+      # data='[{"operationName":"users","variables":{"orderBy":{"column":"","order":""},"filter":{"searchText":"","role":"","isActive":true}},"query":"query users($orderBy: OrderByUserInput, $filter: FilterUserInput) {  users(orderBy: $orderBy, filter: $filter) {    ...UserInfo    __typename  }}fragment UserInfo on User {  id  username  role  isActive  email  profile {    ...UserProfileInfo    __typename  }  auth {    apikey {      apikey      __typename    }    certificate {      serial      __typename    }    facebook {      fbId      displayName      __typename    }    google {      googleId      displayName      __typename    }    github {      ghId      displayName      __typename    }    linkedin {      lnId      displayName      __typename    }    __typename  }  __typename}fragment UserProfileInfo on UserProfile {  firstName  middleName  lastName  title  suffix  fullName  __typename}"}]'
       )
   return json.loads(response.text)
