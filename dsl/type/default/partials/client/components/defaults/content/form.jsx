37e3f5e72cfbb30b6e43a00272203e67a9b09219
Tony Worm <verdverm@gmail.com>
2019-02-13 15:30:39 -0700

    fix the default forms for correct handler

--- dsl/type/default/partials/client/components/defaults/content/form.jsx
+++ dsl/type/default/partials/client/components/defaults/content/form.jsx
@@ -1,14 +1,22 @@
-onSubmit = ({{camel TYPE.name}}, {{camel TYPE.name}}Update) => values => {
-  {{camel TYPE.name}}Update({{camel TYPE.name}}.id, values);
-};
+onSubmit = ({{camel TYPE.name}}) => values => {
+  let formMode = this.props.formMode;
+  if (formMode === "create") {
+    this.props.{{camel TYPE.name}}Create(values);
+  } else if (formMode === "update") {
+    this.props.{{camel TYPE.name}}Update({{camel TYPE.name}}.id, values);
+  } else {
+    console.error("Unknown formMode", formMode);
+  }
+}
 
 render() {
   console.log("{{TYPE.name}} - {{COMPONENT.name}} - default content - props", this.props)
-  let { loading{{camelT TYPE.name}}, {{camel TYPE.name}}, {{camel TYPE.name}}Update } = this.props;
+  let { loading{{camelT TYPE.name}}, {{camel TYPE.name}} } = this.props;
+
   return (
     <div>
       <b>{{TYPE.name}} - {{COMPONENT.name}} component - default content </b>
-			<DefaultForm onSubmit={this.onSubmit({{camel TYPE.name}}, {{camel TYPE.name}}Update)} {...this.props} />
+			<DefaultForm onSubmit={ this.onSubmit({{camel TYPE.name}}) } {...this.props} />
       <pre>{ loading{{camelT TYPE.name}} ? "loading" : JSON.stringify({{camel TYPE.name}}, null, 2) }</pre>
     </div>
   )
